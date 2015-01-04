1c3e3ed06656e27431c04cdd6df9b10d19946990
bingo <bingo_zhang520@163.com>
2015-01-04 13:35:11 +0800

    完成hosts的搜索功能

--- sys/controllers/hosts.js
+++ sys/controllers/hosts.js
@@ -9,7 +9,16 @@ module.exports = function(lib) {
       var query = this.query;
       var size = 10;
       var view = this.view;
-      var count = eqmodel.getEquipmentsCount({});
+      var searchValue;
+      if(query.page == null ){
+        searchValue = req.body;
+      }
+      else{
+        searchValue = {ip: query.ip};
+      }
+      //console.log(searchValue);
+
+      var count = eqmodel.getEquipmentsCount(searchValue);  
       var totalpage;
       if(count % 10 > 0){
         totalpage = parseInt(count / 10) + 1;
@@ -17,10 +26,10 @@ module.exports = function(lib) {
       else{
         totalpage = parseInt(count / 10);
       }
-      //console.log(parseInt(query.page, 10));
+
       var page = isNaN(parseInt(query.page, 10)) || parseInt(query.page, 10) > totalpage? 1 : query.page;
-      equipments = eqmodel.getEquipments({
-      }, page, size);
+      equipments = eqmodel.getEquipments(searchValue, page, size);
+
       view.assign('equipments', equipments);
       view.assign('currentpage', page);
       view.assign('totalpage',totalpage);
